78a1a75fa48afb3e9a1fd6f63ff797a196784dda
felipe-luis <105252192+felipe-luis@users.noreply.github.com>
2024-03-29 12:10:30 -0300

    Calculadora em react

---
+++ src/components/Input/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAFFAA;
+        padding: 0 15px;
+
+        text-align: right;
+        border: none;
+        font-size: 24px;
+        overflow: hidden;
+        border-radius: 5px 5px 0 0 ;
+    }
+`;
